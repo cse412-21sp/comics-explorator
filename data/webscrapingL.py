a43d678c83cad0716446262a10b8110ff082f503
mhe233@uw.edu <1910439476@qq.com>
2021-05-28 20:07:10 -0700

    added two new visualizations and changed header display.

--- data/webscrapingL.py
+++ data/webscrapingL.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 data = {'Date': [], 'Name': [], 'Publisher': [], 'Est. Counts': []}
 df = pd.DataFrame(data)
-r = requests.get('https://www.comichron.com/monthlycomicssales/2019/2019-11.html')
+r = requests.get('https://www.comichron.com/monthlycomicssales/2020.html')
 bs = BeautifulSoup(r.content,'html.parser')
 text = bs.find('tbody')
 a = text.find('a')
@@ -15,10 +15,10 @@ tr = text.find_all('tr')
 for i in tr[:50]:
     name = i.find('a').text
     tds = i.find_all('td')
-    date = tds[5].text
-    publisher = tds[6].text
-    counts = tds[7].text
+    date = tds[4].text
+    publisher = tds[5].text
+    counts = tds[6].text
     new_row = {'Date': date, 'Name': name, 'Publisher': publisher, 'Est. Counts': counts}
     df = df.append(new_row, ignore_index=True)
 
-df.to_csv('sales2019Nov.csv')
+df.to_csv('sales_comics_2020.csv')
